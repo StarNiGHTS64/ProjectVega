0527301d9baaefeedc3ee0c27b58937075d143c2
StarNiGHTS <carlos.becerrao@jouanen.com.mx>
2023-09-18 13:00:38 -0600

    Setup

---
+++ sendemail.php
@@ -0,0 +1,30 @@
+<?php
+
+// Define some constants
+define( "RECIPIENT_NAME", "John Doe" );
+define( "RECIPIENT_EMAIL", "someone@mail.com" );
+
+
+// Read the form values
+$success = false;
+$senderName = isset( $_POST['username'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['username'] ) : "";
+$senderEmail = isset( $_POST['email'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['email'] ) : "";
+$phone = isset( $_POST['phone'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['phone'] ) : "";
+$message = isset( $_POST['message'] ) ? preg_replace( "/(From:|To:|BCC:|CC:|Subject:|Content-Type:)/", "", $_POST['message'] ) : "";
+
+// If all values exist, send the email
+if ( $senderName && $senderEmail && $phone && $message) {
+  $recipient = RECIPIENT_NAME . " <" . RECIPIENT_EMAIL . ">";
+  $headers = "From: " . $senderName . " <" . $senderEmail . ">";
+  $success = mail( $recipient, $phone, $message, $headers );
+
+  //Set Location After Successfull Submission
+  header('Location: contact.html');
+}
+
+else{
+	//Set Location After Unsuccessfull Submission
+  	header('Location: index.html');	
+}
+
+?>
